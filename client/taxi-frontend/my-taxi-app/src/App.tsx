0d28d68dcbc68bba5f35d65b1fb18aeae200c558
w3schools-test <test@w3schools.com>
2025-05-19 00:44:16 +0200

    added sample ui

---
+++ client/taxi-frontend/my-taxi-app/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-yellow-500 text-white p-4 rounded shadow">
+      <h1 className="text-3xl font-bold">Taxi läuft!</h1>
+    </div>
+  );
+}
